d3b70fa097362c03c42c997845f13d6eb3c7abc8
CubeDenmark <cubdesdenmark06242gmail.com>
2024-11-06 12:57:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Button label="Click Me" @click="handleClick" />
+  </div>
+</template>
+
+<script setup>
+import Button from 'primevue/button';
+
+const butt = handleClick(() => alert('Button clicked!'))
+
+</script>
+
+<style>
+/* Optional: Customize button styles */
+</style>
